f8fddd78149023692e960e636469b973af385e35
Mayank Gupta <mayankgupta270606@gmail.com>
2026-04-15 19:13:04 +0530

    Remove unused import of User model in views.py

--- apps/users/views.py
+++ apps/users/views.py
@@ -5,7 +5,7 @@ from rest_framework.views import APIView
 from rest_framework_simplejwt.tokens import RefreshToken
 
 from .serializers import UserLogoutSerializer, UserSignupSerializer ,UserLoginSerializer
-from .models import User
+
 
 
 class UserSignupView(APIView):
